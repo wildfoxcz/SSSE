fb128d35ddc441007b47362cea5ef143a2b1dc34
Jiří Furda <magiecz@gmail.com>
2021-11-05 00:25:05 +0100

    Added page detail

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ Route::get('/', 'HomepageController@index');
 
 Route::get('/obory/{field}', 'FieldController@show')->name('fields.show');
 Route::get('/aktuality/{post}', 'PostController@show')->name('posts.show');
+Route::get('/stranky/{pages}', 'PageController@show')->name('pages.show');
 
 Route::get('ckeditor', 'CkeditorController@index');
 Route::post('ckeditor/upload', 'CkeditorController@upload')->name('ckeditor.upload');
